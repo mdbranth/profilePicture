b62c5949fd31519de1cdce82ecf5bc34b9861861
Matt Branthwaite <mdbranth@gmail.com>
2014-10-09 15:08:21 -0700

    adding default urls

--- profilePicture.js
+++ profilePicture.js
@@ -7,11 +7,11 @@ function getProfilePicture($, _, options) {
   });
 
   var style = options.imgUrl && "background-image: url(" + options.imgUrl + ");" || "";
-  var elt = $('<a>', {'class': 'ef-profile-image', target: '_blank', href: options.url}).append(
+  var elt = $('<a>', {'class': 'ef-profile-image', target: '_blank', href: options.url || '#'}).append(
     $('<div>', {'style': style}).append(
       !options.imgUrl && $('<div>', {'class': 'ef-initials'}).append(initials),
-      $('<a>', {'class': 'ef-times', target: '_blank', href: options.timeUrl}).append($('<i>', {'class': 'fa fa-times'})),
-      $('<a>', {'class': 'ef-check', target: '_blank', href: options.checkUrl}).append($('<i>', {'class': 'fa fa-check'}))
+      $('<a>', {'class': 'ef-times', target: '_blank', href: options.timeUrl || '#'}).append($('<i>', {'class': 'fa fa-times'})),
+      $('<a>', {'class': 'ef-check', target: '_blank', href: options.checkUrl || '#'}).append($('<i>', {'class': 'fa fa-check'}))
     )
   );
   return elt;
